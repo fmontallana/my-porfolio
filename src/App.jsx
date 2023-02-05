7009834479169aa42e96a98cf76ef9e034104e1b
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-05 23:09:59 +0800

    change css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
     <div className="App">
       <h2>Fernando Montallana Jr</h2>
       <h3>My Projects</h3>
-      <ul>
+      <ul style={{
+        listStyleType: 'none',
+        padding: 0,
+        margin: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'start',
+        justifyContent: 'start',
+        textAlign: 'left'
+      }}>
         <li>Anime Streaming  - <a href="http://anime.fermontallana.studio">http://anime.fermontallana.studio</a></li>
         <li>Quote Generator - <a href="http://quote.fermontallana.studio">http://quote.fermontallana.studio</a></li>
         <li>FrostCity HVAC - <a href="http://frostcity-capstone.vercel.app">http://frostcity-capstone.vercel.app</a></li>
