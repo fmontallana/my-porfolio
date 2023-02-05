d4f22768e76c7a51ba8437651a5045bf842e420c
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-05 22:56:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Fernando Montallana Jr</h2>
+      <h3>My Projects</h3>
+      <ul>
+        <li>Anime Streaming  - <a href="http://anime.fermontallana.studio">http://anime.fermontallana.studio</a></li>
+        <li>Quote Generator - <a href="http://quote.fermontallana.studio">http://quote.fermontallana.studio</a></li>
+        <li>FrostCity HVAC - <a href="http://frostcity-capstone.vercel.app">http://frostcity-capstone.vercel.app</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
